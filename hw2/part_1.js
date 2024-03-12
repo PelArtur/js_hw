6d705db06e0a7a9577f0c298101998640be4542e
Artur Pelcharskyi <pelcharskyi.pn@ucu.edu.ua>
2024-03-12 18:08:52 +0200

    part 1

---
+++ hw2/part_1.js
@@ -0,0 +1,58 @@
+function max(arr)
+{
+    if (arr.length === 0) return NaN;
+    let max = arr[0];
+
+    for (let i = 1; i < arr.length; i++)
+    {
+        const current = arr[i];
+
+        if (current > max)
+            max = current;
+    }
+
+    return max;
+}
+
+console.log(max([1, 2, 3, 4, 5])) // 5
+console.log(max([])) // NaN
+
+// You can imagine that arr is padded with zeroes from both sides.
+function maxPair(arr)
+{
+    if (arr.length === 0) return NaN;
+
+    let max = [];
+
+    for (let i = 0; i < arr.length; i++)
+    {
+        const current = arr[i];
+        const next = arr[i + 1] || 0;
+
+        if (max.length === 0 || current + next > max[0] + max[1])
+            max = [current, next];
+    }
+
+    return max
+}
+
+console.log(maxPair([1, 2, 3, 4, 5])); // [4, 5]
+console.log(maxPair([1, 2, 3, -4, 10])); // [10, 0]
+console.log(maxPair([10, 1, 2, 3, -4, 4])); // [0, 10]   pair [10, 1] has bigger sum
+console.log(maxPair([1, 2, -3, 10, -4, 4])); // [-3, 10]
+
+function recordProgress(object, tuple)
+{
+    if (tuple.length === 0) return object;
+
+    const ageChange = tuple[0];
+    const weightChange = tuple[1];
+
+    return {
+        ...object,
+        age: (object.age || 0) + ageChange,
+        weight: (object.weight || 0) + weightChange
+    };
+}
+
+console.log(recordProgress({ name: 'Mike', age: 13, weight: 100 }, [1, 20])); // { name: 'Mike', age: 14, weight: 120 }
