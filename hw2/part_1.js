ca2b6f8f3872bf3d631296a8f65fbad643e98c7d
Artur Pelcharskyi <pelcharskyi.pn@ucu.edu.ua>
2024-05-23 18:55:19 +0300

    hw2 part 1 add destructuring

--- hw2/part_1.js
+++ hw2/part_1.js
@@ -41,18 +41,19 @@ console.log(maxPair([1, 2, 3, -4, 10])); // [10, 0]
 console.log(maxPair([10, 1, 2, 3, -4, 4])); // [0, 10]   pair [10, 1] has bigger sum
 console.log(maxPair([1, 2, -3, 10, -4, 4])); // [-3, 10]
 
-function recordProgress(object, tuple)
-{
+function recordProgress(object, tuple) {
     if (tuple.length === 0) return object;
 
-    const ageChange = tuple[0];
-    const weightChange = tuple[1];
+    const { age = 0, weight = 0 } = object;
 
-    return {
-        ...object,
-        age: (object.age || 0) + ageChange,
-        weight: (object.weight || 0) + weightChange
+    const [ageChange, weightChange] = tuple;
+
+    const result = {
+        age: age + ageChange,
+        weight: weight + weightChange
     };
+
+    return { ...object, ...result };
 }
 
 console.log(recordProgress({ name: 'Mike', age: 13, weight: 100 }, [1, 20])); // { name: 'Mike', age: 14, weight: 120 }
